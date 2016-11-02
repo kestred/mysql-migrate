4b35ff5e64df763147bf1cb0295dc699113d05ca
Kestred <kestred@riotcave.com>
2016-11-01 19:09:12 -0700

    Add example directory with example schema

---
+++ example/setup.sql
@@ -0,0 +1,2 @@
+source example/stable.sql
+source example/v1.0.0/migrate.sql
---
+++ example/v1.0.0/migrate.sql
@@ -0,0 +1,5 @@
+CREATE TABLE my_first_table (
+  `id` char(36) NOT NULL,
+  `my_column` int NOT NULL,
+  PRIMARY KEY (`id`)
+);
